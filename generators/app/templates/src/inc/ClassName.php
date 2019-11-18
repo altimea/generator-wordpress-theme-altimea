4eabb7980d213e703312fed3af7968aaecca0130
Anibal Copitan <anibal@pprios.com>
2019-11-17 21:49:38 -0500

    [add] function and easy php to start project

--- generators/app/templates/src/inc/ClassName.php
+++ generators/app/templates/src/inc/ClassName.php
@@ -20,10 +20,31 @@ class <%= name_class %>
         add_action('widgets_init', [$this, 'themeLoadSidebars']);
         add_action('widgets_init', [$this, 'themeLoadWidgets']);
         add_filter('upload_mimes', [$this, 'setMimeTypes']);
+        // add_filter('body_class', [$this, 'bodyClasses']);
+
+        // add_action('admin_menu', [$this, 'removeMetaBoxes']);
+        // add_action('admin_init', [$this, 'addFacebookId']);
 
         //add custompost
         // add_action('init', array(new MyCustomPost(), 'register_post_type'));
     }
+
+	/**
+	 * Add input facebook_id to option [general]
+	 * @return void
+	 */
+	public function addFacebookId() {
+		register_setting( 'general', 'facebook_id', 'esc_attr' );
+		add_settings_field( 'facebook_id', '<label for="facebook_id">Facebook Id</label>' , array(&$this, 'field_facebook_id_html') , 'general' );
+	}
+	/**
+	 * html
+	 * @return void
+	 */
+	public function field_facebook_id_html() {
+		$value = get_option( 'facebook_id', '' );
+		echo '<input type="number" id="facebook_id" name="facebook_id" value="' . $value . '" />';
+	}
 
     /**
      * Create Custom Posts
@@ -40,6 +61,22 @@ class <%= name_class %>
     {
 //        $this->navHeadAction = new NavHeadAction();
     }
+
+	/**
+	 * Remove metabox fields for all users
+	 */
+	public function removeMetaBoxes() {
+		// post
+		remove_meta_box( 'postcustom', 'post', 'normal' );
+		// remove_meta_box( 'postexcerpt', 'post', 'normal' );
+		remove_meta_box( 'commentstatusdiv', 'post', 'normal' );
+		// page
+		remove_meta_box( 'postcustom', 'page', 'normal' );
+		remove_meta_box( 'commentstatusdiv', 'page', 'normal' );
+		remove_meta_box( 'commentsdiv', 'page', 'normal' );
+		remove_meta_box( 'revisionsdiv', 'page', 'normal' );
+		remove_meta_box( 'authordiv' , 'page' , 'normal' );
+	}
 
     /**
      * Register Menus
@@ -63,8 +100,13 @@ class <%= name_class %>
 
     public function addImagesSizes() {
 
+		add_theme_support('post-thumbnails');
+
         add_image_size( 'thumbnail_380', 380 );
         add_image_size( 'thumbnail_480', 480 );
+
+		// Indicate widget sidebars can use selective refresh in the Customizer.
+		// add_theme_support( 'customize-selective-refresh-widgets' );
 
     }
 
@@ -82,6 +124,15 @@ class <%= name_class %>
 //            'before_title'  => '<h3 class="promoapp-title upper">',
 //            'after_title'   => '</h3>',
 //        ));
+		register_sidebar( array(
+			'name'          => __( 'Primary Sidebar', '<%= name %>-theme' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your sidebar.', '<%= name %>-theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		) );
     }
 
     public function themeLoadWidgets() {
@@ -113,5 +164,14 @@ class <%= name_class %>
     public function getMyCustomPosts()
     {
 //        return $this->custom_post_type;
+    }
+
+    public function bodyClasses( $classes )
+    {
+		// Adds a class of group-blog to blogs with more than 1 published author.
+		if ( is_multi_author() ) {
+			$classes[] = 'group-blog';
+		}
+		return $classes;
     }
 }
